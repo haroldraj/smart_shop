7605349833596bed6a6bc0800d15628fc4b3f287
Harold <rnarsonharold@gmail.com>
2025-01-21 11:54:28 +0100

    add springdoc library to generate swagger docs

--- src/main/java/com/haroldraja/smart_shop_user/controller/UserController.java
+++ src/main/java/com/haroldraja/smart_shop_user/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping ("/api/v1/users")
+@RequestMapping ("/api/v1/user")
 public class UserController {
     private final UserService userService;
 
